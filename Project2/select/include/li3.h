19a421bbc65c88a6e9bb3d2052d75bf72e24a78c
nike1000 <st505102000@gmail.com>
2016-11-18 22:01:56 +0800

    Single-Process version with single command

---
+++ Project2/select/include/li3.h
@@ -0,0 +1,26 @@
+#ifndef LI3_H
+#define LI3_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct LineNode
+{
+    int linenum;
+    char* cmdline;
+    int pipeto;
+    int pipe_err;
+    int fd_tofile;
+    char* filename;
+    int fd_readout;
+    int fd_writein;
+    struct LineNode* nextPtr;
+}LineNode;
+
+LineNode* create_node(int, char*, int);
+void insert_node(LineNode *, LineNode *);
+void remove_node(LineNode *);
+void print_lists(LineNode *);
+void free_lists(LineNode *);
+
+#endif
---
+++ Project2/select/src/li3.c
@@ -0,0 +1,51 @@
+#include "../include/li3.h"
+
+LineNode* create_node(int linenum, char* data, int pipeto)
+{
+    LineNode* n = (LineNode*)malloc(sizeof(LineNode));
+    n->linenum = linenum;
+	n->cmdline = data;
+    n->pipeto = pipeto;
+    n->pipe_err = 0;
+    n->fd_tofile = -1;
+    n->filename = NULL;
+    n->fd_writein = -1;
+    n->fd_readout = -1;
+    n->nextPtr = NULL;
+    return n;
+}
+
+void insert_node(LineNode* n1, LineNode* n2)
+{
+    n2->nextPtr = n1->nextPtr;
+    n1->nextPtr = n2;
+}
+
+void remove_node(LineNode* n1)
+{
+    n1->nextPtr = n1->nextPtr->nextPtr;
+}
+
+void print_lists(LineNode* lists)
+{
+    LineNode* n = lists;
+    
+    while (n != NULL)
+    {
+        printf("%d:%s,%d ", n->linenum, n->cmdline, n->pipeto);
+        n = n->nextPtr;
+    }
+
+    printf("\n");
+}
+
+void free_lists(LineNode* lists)
+{
+    // 遞迴刪除串列所有節點
+    if (lists->nextPtr != NULL)
+    {
+        free_lists(lists->nextPtr);
+    }
+
+    free(lists);
+}
